cc218363c226559f16258a10588278a2a532c022
take8843 <1995.rohan@gmail.com>
2019-06-04 22:41:19 +0530

    Updates

---
+++ precache-manifest.a07ef96fe39a2a486e9d57c2b6976e12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953e0dae24fcb7c218bb36345ee76c20",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "8dc7ea989e90fcc6bb24",
+    "url": "/quiz/static/css/main.1293fa2b.chunk.css"
+  },
+  {
+    "revision": "c880e1e4acc02948bfd9",
+    "url": "/quiz/static/js/2.23972c23.chunk.js"
+  },
+  {
+    "revision": "8dc7ea989e90fcc6bb24",
+    "url": "/quiz/static/js/main.ba7bdbb5.chunk.js"
+  },
+  {
+    "revision": "5898efb7f0a1a3dff560",
+    "url": "/quiz/static/js/runtime~main.e6ae5a1e.js"
+  }
+]);
